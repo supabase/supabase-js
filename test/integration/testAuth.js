1f126b3bacc2064f19c34dbaf83da4a8669fe7b0
awalias <antwilson@hotmail.co.uk>
2020-07-24 17:02:28 +0800

    chore: fixed auth tests to reflect interface change

--- test/integration/testAuth.js
+++ test/integration/testAuth.js
@@ -8,24 +8,15 @@ chai.use(require('chai-as-promised'))
 const SUPABASE_URL = process.env.SUPABASE_URL || 'http://localhost:1234'
 const SUPABASE_KEY = process.env.SUPABASE_KEY || 'examplekey'
 
-console.log('SUPABASE_KEY', SUPABASE_KEY)
-
 describe('test signing up and logging in as a new user', () => {
-  const supabase = createClient(
-    'https://nuaqAUDnliweFqRoinst.supabase.net',
-    'ucHLLNUUFXVn2o6pRCcQOW82KF0RAs'
-  )
+  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
   const randomEmail = `a${Math.random()}@google.com`
 
   it('should register a new user', async () => {
     try {
       const response = await supabase.auth.signup(randomEmail, '11password')
-      console.log('response', response)
-      const {body:countries} = await supabase.from('countries').select('*').limit(1)
-      console.log('countries', countries)
-      assert(response.email === randomEmail, 'user could not sign up')
+      assert(response.body.email === randomEmail, 'user could not sign up')
     } catch (error) {
-      // console.log('error', error)
       assert(!error, 'sign up returns an error')
     }
   })
@@ -35,7 +26,6 @@ describe('test signing up and logging in as a new user', () => {
       const response = await supabase.auth.login(randomEmail, '11password')
       assert(response.body.access_token !== undefined, 'user could not log in')
     } catch (error) {
-      // console.log('error', error)
       assert(!error, 'log in returns an error')
     }
   })
@@ -45,7 +35,6 @@ describe('test signing up and logging in as a new user', () => {
       const user = await supabase.auth.user()
       assert(user.email === randomEmail, 'user could not be retrieved')
     } catch (error) {
-      // console.log('error', error)
       assert(!error, 'logged in user returns an error')
     }
   })
