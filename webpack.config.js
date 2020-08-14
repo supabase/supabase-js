4304583596583d47cf9ca6f781fc2b1fdb687785
Pham Hieu <phamhieu1998@gmail.com>
2020-08-14 10:25:33 +0800

    rename umd-temp to umd_temp

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './umd-temp/index.js',
+  entry: './umd_temp/index.js',
   output: {
     path: path.resolve(__dirname, 'umd'),
     filename: 'supabase.js',
