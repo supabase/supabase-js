618b0f86d3a5f39c71caa39bd75fcedc6b05df38
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-01-03 07:38:43 +0900

    Exporting gotrue User as AuthUser instead of User to avoid potential conflicts.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import SupabaseClient from './SupabaseClient'
 import { SupabaseClientOptions, SupabaseRealtimePayload } from './lib/types'
-import { User } from '@supabase/gotrue-js'
+import { User as AuthUser } from '@supabase/gotrue-js'
 export * from '@supabase/gotrue-js'
 export * from '@supabase/realtime-js'
 
@@ -15,4 +15,4 @@ const createClient = (
   return new SupabaseClient(supabaseUrl, supabaseKey, options)
 }
 
-export { createClient, SupabaseClient, SupabaseClientOptions, SupabaseRealtimePayload, User }
+export { createClient, SupabaseClient, SupabaseClientOptions, SupabaseRealtimePayload, AuthUser }
