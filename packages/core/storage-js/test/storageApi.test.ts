a4cc19faaeebd41050a69de3fd9d45a9f735a701
phamhieu <phamhieu1998@gmail.com>
2021-04-26 08:57:12 +0800

    fix: infra docker config

--- packages/core/storage-js/test/storageApi.test.ts
+++ packages/core/storage-js/test/storageApi.test.ts
@@ -1,9 +1,9 @@
 import { StorageBucketApi } from '../src/lib'
 
 // TODO: need to setup storage-api server for this test
-const URL = 'http://0.0.0.0:3000/storage/v1'
+const URL = 'http://localhost:8000/storage/v1'
 const KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMzUzMTk4NSwiZXhwIjoxOTI5MTA3OTg1fQ.wgI6mYLNP38unZyecrhjFe3pnMA2oBuvuH3o58fDJV4'
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJhdWQiOiIiLCJzdWIiOiIiLCJSb2xlIjoicG9zdGdyZXMifQ.magCcozTMKNrl76Tj2dsM7XTl_YH0v0ilajzAvIlw3U'
 
 const storage = new StorageBucketApi(URL, { Authorization: `Bearer ${KEY}` })
 const newBucketName = `my-new-bucket-${Date.now()}`
@@ -20,21 +20,23 @@ test('Get all buckets', async () => {
   expect(res.data).toMatchSnapshot()
 })
 
-// test('Get bucket by id', async () => {
-//   const res = await storage.getBucket('7078bc23-9dd6-460d-8b93-082254fee63a')
-//   expect(res.data).toMatchSnapshot()
-// })
+test('Get bucket by id', async () => {
+  const res = await storage.getBucket('bucket2')
+  expect(res.data).toMatchSnapshot()
+})
 
-// test('Get bucket with wrong id', async () => {
-//   const res = await storage.getBucket('not-exist-id')
-//   expect(res.error).toMatchSnapshot()
-// })
+test('Get bucket with wrong id', async () => {
+  const res = await storage.getBucket('not-exist-id')
+  expect(res.error).toMatchSnapshot()
+})
 
-// test('create new bucket', async () => {
-//   const res = await storage.createBucket(newBucketName)
-//   createdBucketId = res.data!.id
-//   expect(res.data!.name).toEqual(newBucketName)
-// })
+test('create new bucket', async () => {
+  const res = await storage.createBucket(newBucketName)
+  console.log(res.error)
+  console.log(res.data)
+  createdBucketId = res.data!.id
+  expect(res.data!.name).toEqual(newBucketName)
+})
 
 // test('empty bucket', async () => {
 //   const res = await storage.emptyBucket(createdBucketId)
