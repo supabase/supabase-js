8775930d56ea397263c7b6243c71be47b8219c79
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-06-01 07:26:28 +0900

    feat: Added basic tests

---
+++ packages/core/storage-js/test/storageFileApi.test.ts
@@ -0,0 +1,16 @@
+import { SupabaseStorageClient } from '../src/index'
+
+// TODO: need to setup storage-api server for this test
+const URL = 'http://localhost:8000/storage/v1'
+const KEY =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJhdWQiOiIiLCJzdWIiOiIzMTdlYWRjZS02MzFhLTQ0MjktYTBiYi1mMTlhN2E1MTdiNGEiLCJSb2xlIjoicG9zdGdyZXMifQ.pZobPtp6gDcX0UbzMmG3FHSlg4m4Q-22tKtGWalOrNo'
+
+const storage = new SupabaseStorageClient(URL, { Authorization: `Bearer ${KEY}` })
+const newBucketName = `my-new-bucket-${Date.now()}`
+
+test('get public URL', async () => {
+  const res = storage.from(newBucketName).getPublicUrl('profiles/myUniqueUserId/profile.png')
+  expect(res.error).toBeNull()
+  expect(res.data).toMatchSnapshot()
+  expect(res.publicURL).toMatchSnapshot()
+})
