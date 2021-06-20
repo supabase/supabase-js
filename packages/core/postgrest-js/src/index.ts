db3a70b4157a8bcb8c2cdf888e4024bdc9ef1d6d
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-06-20 13:32:55 +0800

    feat: export error and response types

--- packages/core/postgrest-js/src/index.ts
+++ packages/core/postgrest-js/src/index.ts
@@ -1,6 +1,21 @@
 import PostgrestClient from './PostgrestClient'
 import PostgrestFilterBuilder from './lib/PostgrestFilterBuilder'
 import PostgrestQueryBuilder from './lib/PostgrestQueryBuilder'
-import { PostgrestBuilder } from './lib/types'
+import {
+  PostgrestBuilder,
+  PostgrestResponse,
+  PostgrestSingleResponse,
+  PostgrestMaybeSingleResponse,
+  PostgrestError,
+} from './lib/types'
 
-export { PostgrestClient, PostgrestFilterBuilder, PostgrestQueryBuilder, PostgrestBuilder }
+export {
+  PostgrestClient,
+  PostgrestBuilder,
+  PostgrestQueryBuilder,
+  PostgrestFilterBuilder,
+  PostgrestResponse,
+  PostgrestSingleResponse,
+  PostgrestMaybeSingleResponse,
+  PostgrestError,
+}
