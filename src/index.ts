ebb9bb293c9843302629687850020a2d411a05ed
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-06-28 10:51:15 +0800

    fix(postgrest-js): export types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import SupabaseClient from './SupabaseClient'
 import { SupabaseClientOptions, SupabaseRealtimePayload } from './lib/types'
 import { User as AuthUser, Session as AuthSession } from '@supabase/gotrue-js'
 export * from '@supabase/gotrue-js'
+export {
+  PostgrestResponse,
+  PostgrestSingleResponse,
+  PostgrestMaybeSingleResponse,
+  PostgrestError,
+} from '@supabase/postgrest-js'
 export * from '@supabase/realtime-js'
 
 /**
