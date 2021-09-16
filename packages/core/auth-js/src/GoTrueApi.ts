muthhukumar/gotrue-js

feat: allow clients to provide an hcaptcha response token

src/GoTrueApi.ts
@@ -41,6 +41,7 @@ export default class GoTrueApi {
     password: string,
     options: {
       redirectTo?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{ data: Session | User | null; error: Error | null }> {
     try {
@@ -49,7 +50,11 @@ export default class GoTrueApi {
       if (options.redirectTo) {
         queryString = '?redirect_to=' + encodeURIComponent(options.redirectTo)
       }
-      const data = await post(`${this.url}/signup${queryString}`, { email, password }, { headers })
+      const data = await post(
+        `${this.url}/signup${queryString}`,
+        { email, password, hcaptchaToken: options.hcaptchaToken },
+        { headers }
+      )
       let session = { ...data }
       if (session.expires_in) session.expires_at = expiresAt(data.expires_in)
       return { data: session, error: null }
@@ -69,6 +74,7 @@ export default class GoTrueApi {
     password: string,
     options: {
       redirectTo?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{ data: Session | null; error: Error | null }> {
     try {
@@ -77,7 +83,11 @@ export default class GoTrueApi {
       if (options.redirectTo) {
         queryString += '&redirect_to=' + encodeURIComponent(options.redirectTo)
       }
-      const data = await post(`${this.url}/token${queryString}`, { email, password }, { headers })
+      const data = await post(
+        `${this.url}/token${queryString}`,
+        { email, password, hcaptchaToken: options.hcaptchaToken },
+        { headers }
+      )
       let session = { ...data }
       if (session.expires_in) session.expires_at = expiresAt(data.expires_in)
       return { data: session, error: null }
@@ -93,11 +103,18 @@ export default class GoTrueApi {
    */
   async signUpWithPhone(
     phone: string,
-    password: string
+    password: string,
+    options: {
+      hcaptchaToken?: string
+    } = {}
   ): Promise<{ data: Session | User | null; error: Error | null }> {
     try {
       let headers = { ...this.headers }
-      const data = await post(`${this.url}/signup`, { phone, password }, { headers })
+      const data = await post(
+        `${this.url}/signup`,
+        { phone, password, hcaptchaToken: options.hcaptchaToken },
+        { headers }
+      )
       let session = { ...data }
       if (session.expires_in) session.expires_at = expiresAt(data.expires_in)
       return { data: session, error: null }
@@ -113,12 +130,19 @@ export default class GoTrueApi {
    */
   async signInWithPhone(
     phone: string,
-    password: string
+    password: string,
+    options: {
+      hcaptchaToken?: string
+    } = {}
   ): Promise<{ data: Session | null; error: Error | null }> {
     try {
       let headers = { ...this.headers }
       let queryString = '?grant_type=password'
-      const data = await post(`${this.url}/token${queryString}`, { phone, password }, { headers })
+      const data = await post(
+        `${this.url}/token${queryString}`,
+        { phone, password, hcaptchaToken: options.hcaptchaToken },
+        { headers }
+      )
       let session = { ...data }
       if (session.expires_in) session.expires_at = expiresAt(data.expires_in)
       return { data: session, error: null }
@@ -136,6 +160,7 @@ export default class GoTrueApi {
     email: string,
     options: {
       redirectTo?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{ data: {} | null; error: Error | null }> {
     try {
@@ -144,7 +169,11 @@ export default class GoTrueApi {
       if (options.redirectTo) {
         queryString += '?redirect_to=' + encodeURIComponent(options.redirectTo)
       }
-      const data = await post(`${this.url}/magiclink${queryString}`, { email }, { headers })
+      const data = await post(
+        `${this.url}/magiclink${queryString}`,
+        { email, hcaptchaToken: options.hcaptchaToken },
+        { headers }
+      )
       return { data, error: null }
     } catch (error) {
       return { data: null, error }
@@ -155,10 +184,19 @@ export default class GoTrueApi {
    * Sends a mobile OTP via SMS. Will register the account if it doesn't already exist
    * @param phone The user's phone number WITH international prefix
    */
-  async sendMobileOTP(phone: string): Promise<{ data: {} | null; error: Error | null }> {
+  async sendMobileOTP(
+    phone: string,
+    options: {
+      hcaptchaToken?: string
+    } = {}
+  ): Promise<{ data: {} | null; error: Error | null }> {
     try {
       let headers = { ...this.headers }
-      const data = await post(`${this.url}/otp`, { phone }, { headers })
+      const data = await post(
+        `${this.url}/otp`,
+        { phone, hcaptchaToken: options.hcaptchaToken },
+        { headers }
+      )
       return { data, error: null }
     } catch (error) {
       return { data: null, error }
@@ -224,6 +262,7 @@ export default class GoTrueApi {
     email: string,
     options: {
       redirectTo?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{ data: {} | null; error: Error | null }> {
     try {
@@ -232,7 +271,11 @@ export default class GoTrueApi {
       if (options.redirectTo) {
         queryString += '?redirect_to=' + encodeURIComponent(options.redirectTo)
       }
-      const data = await post(`${this.url}/recover${queryString}`, { email }, { headers })
+      const data = await post(
+        `${this.url}/recover${queryString}`,
+        { email, hcaptchaToken: options.hcaptchaToken },
+        { headers }
+      )
       return { data, error: null }
     } catch (error) {
       return { data: null, error }

src/GoTrueClient.ts
@@ -99,6 +99,7 @@ export default class GoTrueClient {
     { email, password, phone }: UserCredentials,
     options: {
       redirectTo?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{
     user: User | null
@@ -111,10 +112,13 @@ export default class GoTrueClient {
 
       const { data, error } =
         phone && password
-          ? await this.api.signUpWithPhone(phone!, password!)
+          ? await this.api.signUpWithPhone(phone!, password!, {
+            hcaptchaToken: options.hcaptchaToken,
+          })
           : await this.api.signUpWithEmail(email!, password!, {
-              redirectTo: options.redirectTo,
-            })
+            redirectTo: options.redirectTo,
+            hcaptchaToken: options.hcaptchaToken,
+          })
 
       if (error) {
         throw error
@@ -159,6 +163,7 @@ export default class GoTrueClient {
     options: {
       redirectTo?: string
       scopes?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{
     session: Session | null
@@ -174,6 +179,7 @@ export default class GoTrueClient {
       if (email && !password) {
         const { error } = await this.api.sendMagicLinkEmail(email, {
           redirectTo: options.redirectTo,
+          hcaptchaToken: options.hcaptchaToken,
         })
         return { data: null, user: null, session: null, error }
       }
@@ -183,7 +189,9 @@ export default class GoTrueClient {
         })
       }
       if (phone && !password) {
-        const { error } = await this.api.sendMobileOTP(phone)
+        const { error } = await this.api.sendMobileOTP(phone, {
+          hcaptchaToken: options.hcaptchaToken,
+        })
         return { data: null, user: null, session: null, error }
       }
       if (phone && password) {
@@ -446,9 +454,10 @@ export default class GoTrueClient {
    * Receive a notification every time an auth event happens.
    * @returns {Subscription} A subscription object which can be used to unsubscribe itself.
    */
-  onAuthStateChange(
-    callback: (event: AuthChangeEvent, session: Session | null) => void
-  ): { data: Subscription | null; error: Error | null } {
+  onAuthStateChange(callback: (event: AuthChangeEvent, session: Session | null) => void): {
+    data: Subscription | null
+    error: Error | null
+  } {
     try {
       const id: string = uuid()
       const self = this

supabase/supabase-js

packages/core/auth-js/src/GoTrueApi.ts
@@ -48,6 +48,7 @@ export default class GoTrueApi {
     options: {
       redirectTo?: string
       data?: object
+      hcaptchaToken?: string
     } = {}
   ): Promise<{ data: Session | User | null; error: ApiError | null }> {
     try {
@@ -58,7 +59,12 @@ export default class GoTrueApi {
       }
       const data = await post(
         `${this.url}/signup${queryString}`,
-        { email, password, data: options.data },
+        {
+          email,
+          password,
+          data: options.data,
+          gotrue_meta_security: { hcaptcha_token: options.hcaptchaToken },
+        },
         { headers }
       )
       let session = { ...data }
@@ -108,13 +114,19 @@ export default class GoTrueApi {
     password: string,
     options: {
       data?: object
+      hcaptchaToken?: string
     } = {}
   ): Promise<{ data: Session | User | null; error: ApiError | null }> {
     try {
       let headers = { ...this.headers }
       const data = await post(
         `${this.url}/signup`,
-        { phone, password, data: options.data },
+        {
+          phone,
+          password,
+          data: options.data,
+          gotrue_meta_security: { hcaptcha_token: options.hcaptchaToken },
+        },
         { headers }
       )
       let session = { ...data }
@@ -155,6 +167,7 @@ export default class GoTrueApi {
     email: string,
     options: {
       redirectTo?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{ data: {} | null; error: ApiError | null }> {
     try {
@@ -163,7 +176,11 @@ export default class GoTrueApi {
       if (options.redirectTo) {
         queryString += '?redirect_to=' + encodeURIComponent(options.redirectTo)
       }
-      const data = await post(`${this.url}/magiclink${queryString}`, { email }, { headers })
+      const data = await post(
+        `${this.url}/magiclink${queryString}`,
+        { email, gotrue_meta_security: { hcaptcha_token: options.hcaptchaToken } },
+        { headers }
+      )
       return { data, error: null }
     } catch (error) {
       return { data: null, error }
@@ -174,10 +191,19 @@ export default class GoTrueApi {
    * Sends a mobile OTP via SMS. Will register the account if it doesn't already exist
    * @param phone The user's phone number WITH international prefix
    */
-  async sendMobileOTP(phone: string): Promise<{ data: {} | null; error: ApiError | null }> {
+  async sendMobileOTP(
+    phone: string,
+    options: {
+      hcaptchaToken?: string
+    } = {}
+  ): Promise<{ data: {} | null; error: ApiError | null }> {
     try {
       let headers = { ...this.headers }
-      const data = await post(`${this.url}/otp`, { phone }, { headers })
+      const data = await post(
+        `${this.url}/otp`,
+        { phone, gotrue_meta_security: { hcaptcha_token: options.hcaptchaToken } },
+        { headers }
+      )
       return { data, error: null }
     } catch (error) {
       return { data: null, error }
@@ -249,6 +275,7 @@ export default class GoTrueApi {
     email: string,
     options: {
       redirectTo?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{ data: {} | null; error: ApiError | null }> {
     try {
@@ -257,7 +284,11 @@ export default class GoTrueApi {
       if (options.redirectTo) {
         queryString += '?redirect_to=' + encodeURIComponent(options.redirectTo)
       }
-      const data = await post(`${this.url}/recover${queryString}`, { email }, { headers })
+      const data = await post(
+        `${this.url}/recover${queryString}`,
+        { email, gotrue_meta_security: { hcaptcha_token: options.hcaptchaToken } },
+        { headers }
+      )
       return { data, error: null }
     } catch (error) {
       return { data: null, error }

packages/core/auth-js/src/GoTrueClient.ts
@@ -29,8 +29,8 @@ type MaybePromisify<T> = T | Promise<T>
 
 type PromisifyMethods<T> = {
   [K in keyof T]: T[K] extends AnyFunction
-    ? (...args: Parameters<T[K]>) => MaybePromisify<ReturnType<T[K]>>
-    : T[K]
+  ? (...args: Parameters<T[K]>) => MaybePromisify<ReturnType<T[K]>>
+  : T[K]
 }
 
 type SupportedStorage = PromisifyMethods<Pick<Storage, 'getItem' | 'setItem' | 'removeItem'>>
@@ -113,6 +113,7 @@ export default class GoTrueClient {
     options: {
       redirectTo?: string
       data?: object
+      hcaptchaToken?: string
     } = {}
   ): Promise<{
     user: User | null
@@ -126,12 +127,14 @@ export default class GoTrueClient {
       const { data, error } =
         phone && password
           ? await this.api.signUpWithPhone(phone!, password!, {
-              data: options.data,
-            })
+            data: options.data,
+            hcaptchaToken: options.hcaptchaToken,
+          })
           : await this.api.signUpWithEmail(email!, password!, {
-              redirectTo: options.redirectTo,
-              data: options.data,
-            })
+            redirectTo: options.redirectTo,
+            data: options.data,
+            hcaptchaToken: options.hcaptchaToken,
+          })
 
       if (error) {
         throw error
@@ -176,6 +179,7 @@ export default class GoTrueClient {
     options: {
       redirectTo?: string
       scopes?: string
+      hcaptchaToken?: string
     } = {}
   ): Promise<{
     session: Session | null
@@ -191,6 +195,7 @@ export default class GoTrueClient {
       if (email && !password) {
         const { error } = await this.api.sendMagicLinkEmail(email, {
           redirectTo: options.redirectTo,
+          hcaptchaToken: options.hcaptchaToken,
         })
         return { data: null, user: null, session: null, error }
       }
@@ -200,7 +205,9 @@ export default class GoTrueClient {
         })
       }
       if (phone && !password) {
-        const { error } = await this.api.sendMobileOTP(phone)
+        const { error } = await this.api.sendMobileOTP(phone, {
+          hcaptchaToken: options.hcaptchaToken,
+        })
         return { data: null, user: null, session: null, error }
       }
       if (phone && password) {
@@ -457,9 +464,10 @@ export default class GoTrueClient {
    * Receive a notification every time an auth event happens.
    * @returns {Subscription} A subscription object which can be used to unsubscribe itself.
    */
-  onAuthStateChange(
-    callback: (event: AuthChangeEvent, session: Session | null) => void
-  ): { data: Subscription | null; error: ApiError | null } {
+  onAuthStateChange(callback: (event: AuthChangeEvent, session: Session | null) => void): {
+    data: Subscription | null
+    error: ApiError | null
+  } {
     try {
       const id: string = uuid()
       const self = this