097fdf294a808c46a68c9ee6e40f415ecd7d7ae6
Kai Malcolm <34199284+velocity23@users.noreply.github.com>
2021-11-22 10:18:43 +1100

    Add JWT validation tests

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,9 +1,14 @@
 import { createClient, SupabaseClient } from '../src/index'
 
 const URL = 'http://localhost:3000'
-const KEY = 'some.fake.key'
+const VALID_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTk4NjQxMCwiZXhwIjoxOTUxNTYyNDEwfQ.-uF_c2AEivd8BeMFPDXdCZO12ir61Qvb0jvw4Tm1Z1A'
+const INVALID_KEY = 'some.fake.key'
+const MALFORMED_KEY = 'helloworld'
+const EXPIRED_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MCwiZXhwIjoxfQ.9RFgd6Co8tRYSzBhjPbe9WgJW2HmHHYmV0LZVhfvFus'
 
-const supabase = createClient(URL, KEY)
+const supabase = createClient(URL, VALID_KEY)
 
 test('it should create the client connection', async () => {
   expect(supabase).toBeDefined()
@@ -11,8 +16,22 @@ test('it should create the client connection', async () => {
 })
 
 test('it should throw an error if no valid params are provided', async () => {
-  expect(() => createClient('', KEY)).toThrowError('supabaseUrl is required.')
+  expect(() => createClient('', VALID_KEY)).toThrowError('supabaseUrl is required.')
   expect(() => createClient(URL, '')).toThrowError('supabaseKey is required.')
+})
+
+test('it should throw an error if the JWT is invalid', async () => {
+  expect(() => createClient(URL, INVALID_KEY)).toThrowError('Invalid JWT - JSON not valid.')
+})
+
+test('it should throw an error if the JWT is malformed', async () => {
+  expect(() => createClient(URL, MALFORMED_KEY)).toThrowError('Invalid JWT - Malformed token.')
+})
+
+test('it should throw an error if the JWT is expired', async () => {
+  expect(() => createClient(URL, EXPIRED_KEY)).toThrowError(
+    'supabaseKey must have a future expiry date.'
+  )
 })
 
 // Socket should close when there are no open connections
