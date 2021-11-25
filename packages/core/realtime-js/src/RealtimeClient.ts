78f9f9b7569b17f0252e0440c1efd3fdf3581065
Copple <10214025+kiwicopple@users.noreply.github.com>
2021-11-25 19:17:25 +0800

    fix: only push tokens to joined channels

--- packages/core/realtime-js/src/RealtimeClient.ts
+++ packages/core/realtime-js/src/RealtimeClient.ts
@@ -328,7 +328,7 @@ export default class RealtimeClient {
 
     this.channels.forEach(
       (channel) =>
-        channel.joinedOnce &&
+        channel.isJoined() &&
         channel.push(CHANNEL_EVENTS.access_token, {
           access_token: token,
         })
