a7b62cd4eecc690f1686161cb3525c5bed3c2a0c
teruyuki kobayashi <kobayashi.teruyuki@shanti-robo.co.jp>
2022-01-27 20:04:00 +0900

    add signInWithOpenIDConnect method @J0 @kangmingtay

--- packages/core/auth-js/test/GoTrueClient.test.ts
+++ packages/core/auth-js/test/GoTrueClient.test.ts
@@ -287,7 +287,7 @@ describe('GoTrueClient', () => {
       nonce: 'random value',
       provider: 'google'  
     }
-    const { session, user, error } = await auth.signIn(openIDConnect)
+    const { session, user, error } = await auth.signIn({openIDConnect: openIDConnect})
 
     expect(error).not.toBeNull()
     expect(session).toBeNull()
