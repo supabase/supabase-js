0047e921198b6f129f6a128890245036dd501742
Inian <inian1234@gmail.com>
2022-03-20 23:30:22 +0100

    autogenerated docs

--- packages/core/functions-js/src/index.ts
+++ packages/core/functions-js/src/index.ts
@@ -22,14 +22,19 @@ export class FunctionsClient {
   // do we need a setAuth method?
 
   // can type body via filter RequestInit from https://microsoft.github.io/PowerBI-JavaScript/interfaces/_node_modules_typedoc_node_modules_typescript_lib_lib_dom_d_.requestinit.html
+
+  /**
+   * Invokes a function
+   * @params functionName - the name of the function to invoke
+   * @params body - the body to be sent when invoking the function
+   * @params headers - the headers to be sent when invoking the function
+   */
   async invoke(
     functionName: string,
     headers: { [key: string]: string } = {},
     body?: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string
   ): Promise<{ data: string | null; error: Error | null }> {
     try {
-      console.log(`calling ${functionName}`)
-
       const response = await this.fetch(`${this.url}/${functionName}`, {
         method: 'POST',
         headers: Object.assign({}, headers, this.headers),
