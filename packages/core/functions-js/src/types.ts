dd66afcb206e93acffcbe6000bb23aa44ded0594
Inian <inian1234@gmail.com>
2022-03-22 10:19:02 +0100

    headers are optional

--- packages/core/functions-js/src/types.ts
+++ packages/core/functions-js/src/types.ts
@@ -2,6 +2,6 @@ import crossFetch from 'cross-fetch'
 
 export type Fetch = typeof crossFetch
 export type FunctionInvokeOptions = {
-  headers: { [key: string]: string }
+  headers?: { [key: string]: string }
   body?: Blob | BufferSource | FormData | URLSearchParams | ReadableStream<Uint8Array> | string
 }
