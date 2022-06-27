4fca88ae3cc546998bc232144f20c9dda8515883
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-06-27 14:49:03 +0800

    fix: import cross-fetch conditionally

--- packages/core/auth-js/src/lib/helpers.ts
+++ packages/core/auth-js/src/lib/helpers.ts
@@ -1,4 +1,3 @@
-import crossFetch from 'cross-fetch'
 import { SupportedStorage } from './types'
 
 export function expiresAt(expiresIn: number) {
@@ -34,7 +33,7 @@ export const resolveFetch = (customFetch?: Fetch): Fetch => {
   if (customFetch) {
     _fetch = customFetch
   } else if (typeof fetch === 'undefined') {
-    _fetch = crossFetch as unknown as Fetch
+    _fetch = async (...args) => await (await import('cross-fetch')).fetch(...args)
   } else {
     _fetch = fetch
   }
