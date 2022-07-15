08108d1cfc02092298c404745b4009b97fd253c8
joel@joellee.org <joel@joellee.org>
2022-07-15 15:56:39 +0800

    Revert "test:remove unused test"
    This reverts commit 0e386acee7636199d49d44abb91a219128326317.

--- packages/core/auth-js/test/GoTrueApi.test.ts
+++ packages/core/auth-js/test/GoTrueApi.test.ts
@@ -361,8 +361,8 @@ describe('GoTrueApi', () => {
           redirectTo,
         }
       )
-      expect(user).toEqual({})
-      console.log(error)
+
+      expect(user).toBeNull()
       expect(error?.message).toEqual('User not found')
     })
 
