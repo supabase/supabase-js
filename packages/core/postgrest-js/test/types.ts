387522bff052b13e19192baad98a9559d9a8d51d
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-08-12 17:58:06 +0800

    chore: run prettier

--- packages/core/postgrest-js/test/types.ts
+++ packages/core/postgrest-js/test/types.ts
@@ -1,121 +1,115 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json }
-  | Json[];
+export type Json = string | number | boolean | null | { [key: string]: Json } | Json[]
 
 export interface Database {
   personal: {
     Tables: {
       users: {
         Row: {
-          username: string;
-          data: Json | null;
-          age_range: unknown | null;
-          status: "ONLINE" | "OFFLINE" | null;
-        };
+          username: string
+          data: Json | null
+          age_range: unknown | null
+          status: 'ONLINE' | 'OFFLINE' | null
+        }
         Insert: {
-          username: string;
-          data?: Json | null;
-          age_range?: unknown | null;
-          status?: "ONLINE" | "OFFLINE" | null;
-        };
+          username: string
+          data?: Json | null
+          age_range?: unknown | null
+          status?: 'ONLINE' | 'OFFLINE' | null
+        }
         Update: {
-          username?: string;
-          data?: Json | null;
-          age_range?: unknown | null;
-          status?: "ONLINE" | "OFFLINE" | null;
-        };
-      };
-    };
+          username?: string
+          data?: Json | null
+          age_range?: unknown | null
+          status?: 'ONLINE' | 'OFFLINE' | null
+        }
+      }
+    }
     Functions: {
       get_status: {
-        Args: { name_param: string };
-        Returns: "ONLINE" | "OFFLINE";
-      };
-    };
-  };
+        Args: { name_param: string }
+        Returns: 'ONLINE' | 'OFFLINE'
+      }
+    }
+  }
   public: {
     Tables: {
       users: {
         Row: {
-          username: string;
-          data: Json | null;
-          age_range: unknown | null;
-          catchphrase: unknown | null;
-          status: "ONLINE" | "OFFLINE" | null;
-        };
+          username: string
+          data: Json | null
+          age_range: unknown | null
+          catchphrase: unknown | null
+          status: 'ONLINE' | 'OFFLINE' | null
+        }
         Insert: {
-          username: string;
-          data?: Json | null;
-          age_range?: unknown | null;
-          catchphrase?: unknown | null;
-          status?: "ONLINE" | "OFFLINE" | null;
-        };
+          username: string
+          data?: Json | null
+          age_range?: unknown | null
+          catchphrase?: unknown | null
+          status?: 'ONLINE' | 'OFFLINE' | null
+        }
         Update: {
-          username?: string;
-          data?: Json | null;
-          age_range?: unknown | null;
-          catchphrase?: unknown | null;
-          status?: "ONLINE" | "OFFLINE" | null;
-        };
-      };
+          username?: string
+          data?: Json | null
+          age_range?: unknown | null
+          catchphrase?: unknown | null
+          status?: 'ONLINE' | 'OFFLINE' | null
+        }
+      }
       channels: {
         Row: {
-          id: number;
-          data: Json | null;
-          slug: string | null;
-        };
+          id: number
+          data: Json | null
+          slug: string | null
+        }
         Insert: {
-          id?: number;
-          data?: Json | null;
-          slug?: string | null;
-        };
+          id?: number
+          data?: Json | null
+          slug?: string | null
+        }
         Update: {
-          id?: number;
-          data?: Json | null;
-          slug?: string | null;
-        };
-      };
+          id?: number
+          data?: Json | null
+          slug?: string | null
+        }
+      }
       messages: {
         Row: {
-          id: number;
-          data: Json | null;
-          message: string | null;
-          username: string;
-          channel_id: number;
-        };
+          id: number
+          data: Json | null
+          message: string | null
+          username: string
+          channel_id: number
+        }
         Insert: {
-          id?: number;
-          data?: Json | null;
-          message?: string | null;
-          username: string;
-          channel_id: number;
-        };
+          id?: number
+          data?: Json | null
+          message?: string | null
+          username: string
+          channel_id: number
+        }
         Update: {
-          id?: number;
-          data?: Json | null;
-          message?: string | null;
-          username?: string;
-          channel_id?: number;
-        };
-      };
-    };
+          id?: number
+          data?: Json | null
+          message?: string | null
+          username?: string
+          channel_id?: number
+        }
+      }
+    }
     Functions: {
       get_status: {
-        Args: { name_param: string };
-        Returns: "ONLINE" | "OFFLINE";
-      };
+        Args: { name_param: string }
+        Returns: 'ONLINE' | 'OFFLINE'
+      }
       get_username_and_status: {
-        Args: { name_param: string };
-        Returns: Record<string, unknown>[];
-      };
+        Args: { name_param: string }
+        Returns: Record<string, unknown>[]
+      }
       void_func: {
-        Args: Record<PropertyKey, never>;
-        Returns: undefined;
-      };
-    };
-  };
+        Args: Record<PropertyKey, never>
+        Returns: undefined
+      }
+    }
+  }
 }
