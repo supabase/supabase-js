a0caaafc579220cb3b0fd48931a5e95ae94f4bd7
dng <danny@supabase.io>
2023-01-04 14:10:26 -0800

    Fix comments

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -25,7 +25,7 @@ export type SupabaseClientOptions<SchemaName> = {
      */
     storageKey?: string
     /**
-     * Whether to persist a logged in session to storage.
+     * Whether to persist a logged in session to storage. Defaults to true.
      */
     persistSession?: boolean
     /**
@@ -33,7 +33,7 @@ export type SupabaseClientOptions<SchemaName> = {
      */
     detectSessionInUrl?: boolean
     /**
-     * A storage provider. Used to store the logged in session. Defaults to true.
+     * A storage provider. Used to store the logged-in session.
      */
     storage?: SupabaseAuthClientOptions['storage']
   }
