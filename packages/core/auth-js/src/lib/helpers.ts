3a60056db27dbb3e36ca15340b7f834e981adfbe
Joel Lee <lee.yi.jie.joel@gmail.com>
2023-04-03 23:50:17 +0800

    fix: remove crypto-js (#641)
    ## What kind of change does this PR introduce?
    
    Removes crypto-js entirely as users have reported breaking behaviour
    when used together with Nuxt. Additionally, it seems like subtle crypto
    is supported on a substantial number of frameworks (e.g. Deno, Chrome
    extension under https). As such, we'll be removing it and advising users
    on platforms without native access to the WebCryptoAPI (e.g. React
    Native) to use a polyfill
    
    ---------
    
    Co-authored-by: joel@joellee.org <joel@joellee.org>

--- packages/core/auth-js/src/lib/helpers.ts
+++ packages/core/auth-js/src/lib/helpers.ts
@@ -1,5 +1,4 @@
 import { SupportedStorage } from './types'
-import sha256CryptoJS from 'crypto-js/sha256'
 export function expiresAt(expiresIn: number) {
   const timeNow = Math.round(Date.now() / 1000)
   return timeNow + expiresIn
@@ -245,21 +244,18 @@ function dec2hex(dec: number) {
 export function generatePKCEVerifier() {
   const verifierLength = 56
   const array = new Uint32Array(verifierLength)
-  if (!isBrowser()) {
-    for (let i = 0; i < verifierLength; i++) {
-      array[i] = Math.floor(Math.random() * 256)
-    }
-  } else {
-    window.crypto.getRandomValues(array)
+  if (typeof window.crypto === 'undefined') {
+    throw 'PKCE is not supported on devices without WebCrypto API support or equivalent polyfills'
   }
+  window.crypto.getRandomValues(array)
   return Array.from(array, dec2hex).join('')
 }
 
 async function sha256(randomString: string) {
   const encoder = new TextEncoder()
   const encodedData = encoder.encode(randomString)
-  if (!isBrowser()) {
-    return sha256CryptoJS(randomString).toString()
+  if (typeof window.crypto === 'undefined') {
+    throw 'PKCE is not supported on devices without WebCrypto API support or equivalent polyfills'
   }
   const hash = await window.crypto.subtle.digest('SHA-256', encodedData)
   const bytes = new Uint8Array(hash)
