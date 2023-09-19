ccf4cab47f8e620cf181241e914839865602809e
thorwebdev <thor@supabase.io>
2023-09-19 18:05:48 +0800

    feat: track javascript environment.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,14 @@
 // constants.ts
 import { version } from './version'
-export const DEFAULT_HEADERS = { 'X-Client-Info': `supabase-js/${version}` }
+let JS_ENV = ''
+// @ts-ignore
+if (typeof Deno !== 'undefined') {
+  JS_ENV = 'deno'
+} else if (typeof document !== 'undefined') {
+  JS_ENV = 'web'
+} else if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
+  JS_ENV = 'react-native'
+} else {
+  JS_ENV = 'node'
+}
+export const DEFAULT_HEADERS = { 'X-Client-Info': `supabase-js-${JS_ENV}/${version}` }
