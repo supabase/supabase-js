dd21985a6669c5e0f012dd3639ddf39a3faae7cd
Juan Cardenas <cardenas19890@gmail.com>
2023-10-03 18:58:30 -0500

    fix: typo (#786)

--- packages/core/auth-js/src/GoTrueClient.ts
+++ packages/core/auth-js/src/GoTrueClient.ts
@@ -848,7 +848,7 @@ export default class GoTrueClient {
             try {
               await result
             } catch (e: any) {
-              // we jsut care if it finished
+              // we just care if it finished
             }
           })()
         )
