64c0359ce2d32982c1a67309288f2593fce7828a
Filipe Cabaço <filipe@supabase.io>
2023-10-26 10:30:06 +0100

    fix: Add missing `on` overload for Broadcasts (#261)

--- packages/core/realtime-js/src/RealtimeChannel.ts
+++ packages/core/realtime-js/src/RealtimeChannel.ts
@@ -397,6 +397,15 @@ export default class RealtimeChannel {
       [key: string]: any
     }) => void
   ): RealtimeChannel
+  on<T extends { [key: string]: any }>(
+    type: `${REALTIME_LISTEN_TYPES.BROADCAST}`,
+    filter: { event: string },
+    callback: (payload: {
+      type: `${REALTIME_LISTEN_TYPES.BROADCAST}`
+      event: string
+      payload: T
+    }) => void
+  ): RealtimeChannel
   on(
     type: `${REALTIME_LISTEN_TYPES}`,
     filter: { event: string; [key: string]: string },
