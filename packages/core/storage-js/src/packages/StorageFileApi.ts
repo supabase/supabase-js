f3a652614b9e97d674bd4695f134d02bcb186be3
Hector Ayala <hran@bombillazo.com>
2023-11-14 05:03:07 -0400

    Return ID and full path of uploaded file (#168)
    * feat: return id and full path of uploaded file
    
    * feat: return full path for signed url upload

--- packages/core/storage-js/src/packages/StorageFileApi.ts
+++ packages/core/storage-js/src/packages/StorageFileApi.ts
@@ -68,7 +68,7 @@ export default class StorageFileApi {
     fileOptions?: FileOptions
   ): Promise<
     | {
-        data: { path: string }
+        data: { id: string, path: string, fullPath: string }
         error: null
       }
     | {
@@ -106,13 +106,15 @@ export default class StorageFileApi {
         ...(options?.duplex ? { duplex: options.duplex } : {}),
       })
 
+      const data = await res.json()
+
       if (res.ok) {
         return {
-          data: { path: cleanPath },
+          data: { path: cleanPath, id: data.Id, fullPath: data.Key },
           error: null,
         }
       } else {
-        const error = await res.json()
+        const error = data
         return { data: null, error }
       }
     } catch (error) {
@@ -192,13 +194,15 @@ export default class StorageFileApi {
         headers,
       })
 
+      const data = await res.json()
+
       if (res.ok) {
         return {
-          data: { path: cleanPath },
+          data: { path: cleanPath, fullPath: data.Key },
           error: null,
         }
       } else {
-        const error = await res.json()
+        const error = data
         return { data: null, error }
       }
     } catch (error) {
