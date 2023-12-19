964077f31c2036465aabbeff37cb4974294b5ddc
Rodrigo Mansueli Nunes <rodrigo@mansueli.com>
2023-12-19 10:49:01 -0300

    prettier

--- packages/core/functions-js/src/types.ts
+++ packages/core/functions-js/src/types.ts
@@ -53,21 +53,22 @@ export type FunctionInvokeOptions = {
   /**
    * The Region to invoke the function in.
    */
-  region?: 'any'
-  | 'ap-northeast-1'
-  | 'ap-northeast-2'
-  | 'ap-south-1'
-  | 'ap-southeast-1'
-  | 'ap-southeast-2'
-  | 'ca-central-1'
-  | 'eu-central-1'
-  | 'eu-west-1'
-  | 'eu-west-2'
-  | 'eu-west-3'
-  | 'sa-east-1'
-  | 'us-east-1'
-  | 'us-west-1'
-  | 'us-west-2'
+  region?:
+    | 'any'
+    | 'ap-northeast-1'
+    | 'ap-northeast-2'
+    | 'ap-south-1'
+    | 'ap-southeast-1'
+    | 'ap-southeast-2'
+    | 'ca-central-1'
+    | 'eu-central-1'
+    | 'eu-west-1'
+    | 'eu-west-2'
+    | 'eu-west-3'
+    | 'sa-east-1'
+    | 'us-east-1'
+    | 'us-west-1'
+    | 'us-west-2'
   /**
    * The body of the request.
    */
