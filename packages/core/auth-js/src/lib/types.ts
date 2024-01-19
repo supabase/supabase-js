66c4a00145d8b58b54d2b254a4e55cdcbc1f91bf
Joel Lee <lee.yi.jie.joel@gmail.com>
2024-01-19 18:58:12 +0800

    chore: add friendly name to enroll response (#812)
    ## What kind of change does this PR introduce?
    
    We introduced friendly name in the response of `enroll` a while back:
    https://github.com/supabase/gotrue/pull/1277
    
    This PR adds the field to the client library response
    
    ---------
    
    Co-authored-by: joel@joellee.org <joel@joellee.org>
    Co-authored-by: Stojan Dimitrovski <sdimitrovski@gmail.com>

--- packages/core/auth-js/src/lib/types.ts
+++ packages/core/auth-js/src/lib/types.ts
@@ -862,6 +862,8 @@ export type AuthMFAEnrollResponse =
            * to use it. Avoid loggin this value to the console. */
           uri: string
         }
+        /** Friendly name of the factor, useful for distinguishing between factors **/
+        friendly_name?: string
       }
       error: null
     }
