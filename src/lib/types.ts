7b4c23f85a8e43251fa8fe644870b56b591e5de9
Stojan Dimitrovski <sdimitrovski@gmail.com>
2024-04-01 16:52:58 +0200

    feat: add third-party auth support

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -66,6 +66,18 @@ export type SupabaseClientOptions<SchemaName> = {
      */
     headers?: Record<string, string>
   }
+  /**
+   * Optional function for using a third-party authentication system with
+   * Supabase. The function should return an access token or ID token (JWT) by
+   * obtaining it from the third-party auth client library. Note that this
+   * function may be called concurrently and many times. Use memoization and
+   * locking techniques if this is not supported by the client libraries.
+   *
+   * When set, the `auth` namespace of the Supabase client cannot be used.
+   * Create another client if you wish to use Supabase Auth and third-party
+   * authentications concurrently in the same application.
+   */
+  accessToken?: () => Promise<string>
 }
 
 export type GenericTable = {
