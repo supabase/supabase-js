c1a3116546d86bd9b26eebbb46ab1b2e8131f017
Copple <10214025+kiwicopple@users.noreply.github.com>
2024-12-18 20:58:18 +0100

    adds test

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,9 +1,11 @@
 import { PostgrestClient } from '@supabase/postgrest-js'
-import { createClient, SupabaseClient } from '../src/index'
+import { createClient, createAdminClient, SupabaseClient } from '../src/index'
 import { Database } from './types'
 
 const URL = 'http://localhost:3000'
 const KEY = 'some.fake.key'
+const FAKE_ADMIN_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogInNlcnZpY2Vfcm9sZSIsCiAgImlzcyI6ICJzdXBhYmFzZSIsCiAgImlhdCI6IDE3MzQ0NzY0MDAsCiAgImV4cCI6IDE4OTIyNDI4MDAKfQ.rasWvv_ZRzGaptpahkSyL9R9L6NuEeCUQ45j6_VfYds'
 
 const supabase = createClient(URL, KEY)
 
@@ -17,6 +19,18 @@ test('it should create a client with third-party auth accessToken', async () => 
   expect(() => client.auth.getUser()).toThrowError(
     '@supabase/supabase-js: Supabase Client is configured with the accessToken option, accessing supabase.auth.getUser is not possible'
   )
+})
+
+// Check that it throws an error if a service key is used
+test('createClient() should throw an error if a service key is used', () => {
+  expect(() => createClient(URL, FAKE_ADMIN_KEY)).toThrowError(
+    'createClient() requires an public API key. You are using an admin key. Admin keys should only be used with createAdminClient().'
+  )
+})
+
+// Check that it doesn't throw an error if a service key is used
+test('createAdminClient() should not throw an error if a service key is used', () => {
+  expect(() => createAdminClient(URL, FAKE_ADMIN_KEY)).not.toThrow()
 })
 
 test('it should create the client connection', async () => {
