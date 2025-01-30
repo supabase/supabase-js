bacd8bd707f5d602f5c87d195dda19f52f7d9996
Guilherme Souza <ogrsouza@gmail.com>
2025-01-30 07:12:32 -0300

    chore: upload coverage to coveralls (#217)

--- packages/core/storage-js/jest.config.js
+++ packages/core/storage-js/jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  ollectCoverage: true,
+  coverageDirectory: './test/coverage',
+  coverageReporters: ['lcov'],
+  collectCoverageFrom: [
+    './src/**/*.{js,ts}',
+    './tests/**/*.test.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/vendor/**',
+  ],
 }
