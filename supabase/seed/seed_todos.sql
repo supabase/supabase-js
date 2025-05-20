f254773960f3d5f6558e1d0bf251460742835da6
Guilherme Souza <ogrsouza@gmail.com>
2025-05-20 13:23:06 -0300

    Revert "fix: Bump up realtime-js (#1418)" (#1424)
    This reverts commit 02f2f0b0f797b23c8d8706a7458521f438dbc61c.

--- supabase/seed/seed_todos.sql
+++
@@ -1,8 +0,0 @@
--- Seed data for todos table
-INSERT INTO public.todos (task, is_complete)
-VALUES
-    ('Buy groceries', false),
-    ('Complete project report', true),
-    ('Call mom', false),
-    ('Schedule dentist appointment', false),
-    ('Pay bills', true);
--- test/helper.test.ts
+++ test/helper.test.ts
@@ -1,25 +1,13 @@
-import { cleanUrl } from '../src/lib/helpers'
+import { stripTrailingSlash } from '../src/lib/helpers'
 
 test('Strip trailing slash from URL', () => {
   const URL = 'http://localhost:3000/'
   const expectedURL = URL.slice(0, -1)
-  expect(cleanUrl(URL)).toBe(expectedURL)
+  expect(stripTrailingSlash(URL)).toBe(expectedURL)
 })
 
-test('Return the original URL if there is no slash or white space at the end', () => {
+test('Return the original URL if there is no slash at the end', () => {
   const URL = 'http://localhost:3000'
   const expectedURL = URL
-  expect(cleanUrl(URL)).toBe(expectedURL)
-})
-
-test('Strip trailing white space from URL', () => {
-  const URL = 'http://localhost:3000 '
-  const expectedURL = URL.slice(0, -1)
-  expect(cleanUrl(URL)).toBe(expectedURL)
-})
-
-test('Strip trailing slash followed by white space from URL', () => {
-  const URL = 'http://localhost:3000/ '
-  const expectedURL = URL.slice(0, -2)
-  expect(cleanUrl(URL)).toBe(expectedURL)
+  expect(stripTrailingSlash(URL)).toBe(expectedURL)
 })
