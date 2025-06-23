188c19b0a7046a7ab84551891376cf97e136ce1a
Guilherme Souza <guilherme@supabase.io>
2025-06-23 13:19:22 -0300

    rename to TEST_ANON_KEY

--- examples/expo-app/app/index.tsx
+++ examples/expo-app/app/index.tsx
@@ -3,10 +3,10 @@ import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'http://127.0.0.1:54321'
-const ANON_KEY =
+const TEST_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 
-const supabase = createClient(SUPABASE_URL, ANON_KEY)
+const supabase = createClient(SUPABASE_URL, TEST_ANON_KEY)
 
 export default function Index() {
   const [realtimeStatus, setRealtimeStatus] = useState<string | null>(null)
