f9a26246b258b0bf6785b2f1d90a2efb48135c68
georgiy.rusanov <georgiy.rusanov@ringcentral.com>
2025-07-14 21:48:25 +0200

    no retries for playwright tests

--- test/integration/node-browser/playwright.config.ts
+++ test/integration/node-browser/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
