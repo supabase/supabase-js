54209ab16075b9fe71513ad82985143a6469d9b3
avallete <andrew.valleteau@supabase.io>
2025-08-08 16:10:48 +0200

    chore: add basic types tests for ssr

---
+++ test/integration/next/tests/types/types.test-d.ts
@@ -0,0 +1,129 @@
+import { createServerClient, createBrowserClient } from '@supabase/ssr'
+import { expectType } from 'tsd'
+
+// Copied from ts-expect
+// https://github.com/TypeStrong/ts-expect/blob/master/src/index.ts#L23-L27
+export type TypeEqual<Target, Value> = (<T>() => T extends Target ? 1 : 2) extends <
+  T
+>() => T extends Value ? 1 : 2
+  ? true
+  : false
+
+type Database = {
+  public: {
+    Tables: {
+      shops: {
+        Row: {
+          address: string | null
+          id: number
+          shop_geom: unknown | null
+        }
+        Insert: {
+          address?: string | null
+          id: number
+          shop_geom?: unknown | null
+        }
+        Update: {
+          address?: string | null
+          id?: number
+          shop_geom?: unknown | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+{
+  // createBrowserClient should return a typed client
+  const pg12Client = createBrowserClient<Database>('HTTP://localhost:3000', '')
+  const res12 = await pg12Client.from('shops').select('*')
+  expectType<
+    TypeEqual<
+      | {
+          address: string | null
+          id: number
+          shop_geom: unknown | null
+        }[]
+      | null,
+      typeof res12.data
+    >
+  >(true)
+}
+
+{
+  // createBrowserClient should infer everything to any without types provided
+  const pg12Client = createBrowserClient('HTTP://localhost:3000', '')
+  const res12 = await pg12Client.from('shops').select('address, id, relation(field)')
+  expectType<
+    TypeEqual<
+      | {
+          address: any
+          id: any
+          relation: {
+            field: any
+          }[]
+        }[]
+      | null,
+      typeof res12.data
+    >
+  >(true)
+}
+
+{
+  // createServerClient should return a typed client
+  const pg12Server = createServerClient<Database>('HTTP://localhost:3000', '')
+  const res12 = await pg12Server.from('shops').select('*')
+  expectType<
+    TypeEqual<
+      | {
+          address: string | null
+          id: number
+          shop_geom: unknown | null
+        }[]
+      | null,
+      typeof res12.data
+    >
+  >(true)
+}
+
+{
+  // createServerClient should infer everything to any without types provided
+  const pg12Server = createServerClient('HTTP://localhost:3000', '')
+  const res12 = await pg12Server.from('shops').select('address, id, relation(field)')
+  expectType<
+    TypeEqual<
+      | {
+          address: any
+          id: any
+          relation: {
+            field: any
+          }[]
+        }[]
+      | null,
+      typeof res12.data
+    >
+  >(true)
+}
+
+//  should default to postgrest 12 for untyped client
+{
+  const pg12ServerClient = createServerClient('HTTP://localhost:3000', '')
+  const res12Server = await pg12ServerClient.from('shops').update({ id: 21 }).maxAffected(1)
+  const pg12BrowserClient = createBrowserClient('HTTP://localhost:3000', '')
+  const res12Browser = await pg12BrowserClient.from('shops').update({ id: 21 }).maxAffected(1)
+  expectType<typeof res12Server.Error>('maxAffected method only available on postgrest 13+')
+  expectType<typeof res12Browser.Error>('maxAffected method only available on postgrest 13+')
+}
