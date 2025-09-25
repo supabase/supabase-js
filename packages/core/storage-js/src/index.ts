1ac757e1edcf17b937440f48447bcdd5e36c9009
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2025-09-25 14:47:13 +0300

    fix: export storageClientOptions type (#255)

--- packages/core/storage-js/src/index.ts
+++ packages/core/storage-js/src/index.ts
@@ -1,3 +1,4 @@
-export { StorageClient as StorageClient } from './StorageClient'
+export { StorageClient } from './StorageClient'
+export type { StorageClientOptions } from './StorageClient'
 export * from './lib/types'
 export * from './lib/errors'
