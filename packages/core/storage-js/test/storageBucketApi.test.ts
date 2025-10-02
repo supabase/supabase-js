4565db207ed17f92d43b948497b6cc780b08c60e
tamanyan <tamanyan@users.noreply.github.com>
2025-10-03 01:12:56 +0900

    chore(storage): format code

--- packages/core/storage-js/test/storageBucketApi.test.ts
+++ packages/core/storage-js/test/storageBucketApi.test.ts
@@ -60,11 +60,7 @@ describe('Bucket API Error Handling', () => {
         'http://localhost:1234/storage/v1',
         'support local host with port without modification',
       ],
-      [
-        'http://localhost:1234',
-        'http://localhost:1234',
-        'support local host only with port',
-      ],
+      ['http://localhost:1234', 'http://localhost:1234', 'support local host only with port'],
     ]
 
     urlTestCases.forEach(([inputUrl, expectUrl, description]) => {
