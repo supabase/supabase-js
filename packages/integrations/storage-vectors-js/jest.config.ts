e2a74f3a3d99345d722c8f26bba3b0d6e872bc44
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-10-16 14:31:18 +0300

    fix(repo): small syntax update

--- packages/integrations/storage-vectors-js/jest.config.ts
+++ packages/integrations/storage-vectors-js/jest.config.ts
@@ -1,8 +1,13 @@
 /* eslint-disable */
 import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // Reading the SWC compilation config for the spec files
-const swcJestConfig = JSON.parse(readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8'))
+const swcJestConfig = JSON.parse(readFileSync(join(__dirname, '.spec.swcrc'), 'utf-8'))
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
 swcJestConfig.swcrc = false
