c0796c17ee4d518bbb37ce457faeff81bd686359
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-11-04 16:00:45 +0200

    fix(storage): change test env

--- packages/core/storage-js/jest.config.js
+++ packages/core/storage-js/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: './test/coverage',
   coverageReporters: ['lcov'],
