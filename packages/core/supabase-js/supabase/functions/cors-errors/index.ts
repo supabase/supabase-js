1a418ee49b1b284494e6a174d6299d954c3c3e56
Katerina Skroumpelou <sk.katherine@gmail.com>
2026-01-30 17:35:02 +0200

    fix(supabase): use actual package in deno edge

--- packages/core/supabase-js/supabase/functions/cors-errors/index.ts
+++ packages/core/supabase-js/supabase/functions/cors-errors/index.ts
@@ -1,5 +1,5 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
-import { corsHeaders } from '../../../src/cors.ts'
+import { corsHeaders } from 'npm:@supabase/supabase-js/cors'
 
 serve(async (req) => {
   // Handle CORS preflight
