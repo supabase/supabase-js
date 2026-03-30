05c3658a4a33a3faf52560cac40c7ef68be58a49
Katerina Skroumpelou <sk.katherine@gmail.com>
2026-03-30 18:41:37 +0300

    fix(misc): enable tsdown cjsReexport to resolve dual-module TS2352 type errors

--- packages/core/postgrest-js/tsdown.config.ts
+++ packages/core/postgrest-js/tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: { cjsReexport: true },
   sourcemap: true,
   clean: true,
   outDir: 'dist',
